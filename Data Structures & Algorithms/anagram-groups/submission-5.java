b5e747516b5580f5ac55e74cef7e599f5586bb73
Colin Powers <colinjpowers03@gmail.com>
2026-07-10 20:29:45 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> ans = new HashMap<>();
+        for(String str : strs) {
+            int[] count = new int[26];
+            for(char ch : str.toCharArray()) {
+                count[ch - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            ans.putIfAbsent(key, new ArrayList<>());
+            ans.get(key).add(str);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
