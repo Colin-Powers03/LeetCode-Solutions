7acb00fe6a4f1bb956ca60022af008a01e44755f
Colin Powers <colinjpowers03@gmail.com>
2026-07-18 14:43:36 -0400

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int lp = 0;
+        int rp = numbers.length - 1;
+        while(lp < rp) {
+            if(numbers[lp] + numbers[rp] < target) {
+                lp++;
+            }
+            else if(numbers[lp] + numbers[rp] > target) {
+                rp--;
+            }
+            else {
+                return new int[]{lp + 1, rp + 1};
+            }
+        }
+        return new int[2];
+    }
+}
