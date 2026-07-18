d5c23e07a9b9ab63c24739dc6b3dad3187b9741c
Colin Powers <colinjpowers03@gmail.com>
2026-07-18 14:33:28 -0400

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 0; j < numbers.length; j++) {
+                if(numbers[i] + numbers[j] == target && i != j) {
+                    int[] ans = new int[2];
+                    ans[0] = i + 1;
+                    ans[1] = j + 1;
+                    return ans;
+                }
+            }
+        }
+        return new int[2];
+    }
+}
