7d44b54162b1baf106b4fe05a0aa95ce2849b2b5
Colin Powers <colinjpowers03@gmail.com>
2026-07-27 16:03:40 -0400

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int top = 0;
+        int bot = rows - 1;
+        while(top <= bot) {
+            int row = (top + bot) / 2;
+            if(matrix[row][cols - 1] < target) {
+                top = row + 1;
+            }
+            else if(matrix[row][0] > target) {
+                bot = row - 1;
+            }
+            else {
+                break;
+            }
+        }
+
+        if(!(top <= bot)) {
+            return false;
+        }
+        int row = (top + bot) / 2;
+        int l = 0;
+        int r = cols - 1;
+        while(l <= r) {
+            int mid = l + ((r - l) / 2);
+            if(matrix[row][mid] < target) {
+                l = mid + 1;
+            }
+            else if(matrix[row][mid] > target) {
+                r = mid - 1;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
